9c1ec51c78f19dd8e8a64066fcc84ec3ef1aca5f
Awais Ahmad <aahmad19376@gmail.com>
2025-08-31 14:40:00 +0500

    sync clerk with database

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest, NextResponse } from 'next/server'
 import { createClerkClient } from '@clerk/backend'
